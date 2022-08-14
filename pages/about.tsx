85dc8481ba8c228f021f15eb2e1bdad36545eef6
Minegabu <gabriel.montelle9@gmail.com>
2022-08-15 10:09:43 +1200

    Update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,33 +9,35 @@ const About = () => {
             {
                 session
                     ? (
-                        <div>
-                            <nav>
-                                <a href="/">Home</a>
-                                <a href="/vote">Vote</a>
-                                <a href="/about">About</a>
-                                <a href="/candidates">Candidates</a>
-                            </nav>
+                        <div className="card-overlay">
                             <div>
-                                <div id="profileimg">
-                                    <img src={session.user.image} referrerPolicy="no-referrer" alt="icon" />
+                                <nav>
+                                    <a href="/">Home</a>
+                                    <a href="/vote">Vote</a>
+                                    <a id="selectednav" href="/about">About</a>
+                                    <a href="/candidates">Candidates</a>
+                                    <a href="/"><img src={session.user.image} referrerPolicy="no-referrer" className="img-fluid" alt="userprofile" /></a>
+                                </nav>
+                                <div>
+                                    <h1>Student Council Voting</h1>
+                                    <h2>A website that allows burnside high school students from certain divisions to vote for their student representatives.</h2>
                                 </div>
-                                <h1>Student Council Voting</h1>
-                                <h2>A website that allows burnside high school students from certain divisions to vote for their student representatives.</h2>
                             </div>
                         </div>
                     )
                     : (
                         <>
-                            <nav>
-                                <a href="/">Login as a Student</a>
-                                <a href="/adminlogin">Login as a Admin</a>
-                            </nav>
-                            <div id="login">
-                                <p> Not signed in </p>
-                            </div>
-                            <div id="loginbutton">
-                                <button type="submit" onClick={() => signIn()}>Sign in</button>
+                            <div className="card-overlay">
+                                <nav>
+                                    <a href="/">Login as a Student</a>
+                                    <a href="/adminlogin">Login as a Admin</a>
+                                </nav>
+                                <div id="login">
+                                    <p> Not signed in </p>
+                                </div>
+                                <div id="loginbutton">
+                                    <button type="submit" onClick={() => signIn()}>Sign in</button>
+                                </div>
                             </div>
                         </>
                     )
